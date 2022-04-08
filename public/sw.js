6ef5127610498c9880f9e0000262df45d63e55c3
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2022-04-08 13:21:26 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const staticCacheName = 'Static-Website-v1';
+const assets = [
+    '/',
+    'CSS/style.css',
+    'pleuris.js',
+    'images/logoRijks.png',
+    'error.html',
+];
+
+//Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil( 
+     caches.open(staticCacheName)
+        .then(cache => {
+        console.log('I am caching items!');
+        cache.addAll(assets);
+        })
+    ); 
+});
+
+//Activate event
+self.addEventListener('activate', (event) => {
+    //console.log('The server worker has been activated');
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+                )
+        })
+    )
+});
+
+//Fetch event
+self.addEventListener('fetch', (event)=> {
+    // console.log('An fetch event has taken place', event);
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request)
+        })
+    );
+})
